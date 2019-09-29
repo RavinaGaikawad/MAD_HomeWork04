83a9f581ed9471831b101646e32ec4e3520955dd
sshanbh1 <sshanbh1@uncc.edu>
2019-09-29 18:00:37 -0400

    Remove Merge Conflict and Year Change

---
+++ app/src/main/java/com/example/homework04_api25/ShowListByYear.java
@@ -0,0 +1,19 @@
+package com.example.homework04_api25;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowListByYear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_list_by_year);
+
+        setTitle("Movies By Year");
+
+
+
+    }
+}
